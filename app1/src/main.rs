8e2e5a1b075679d1f841ba22890f422719bb8150
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-09-23 22:54:27 +0200

    Init test project

---
+++ app1/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, Actix Web!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+    })
+    .bind(("0.0.0.0", 9797))?
+    .run()
+    .await
+}
