addb140eba870a84a0eed88377c5f9f5387cbb3e
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-10-05 15:20:29 +0200

    [*] Change response text of app2

--- app2/src/main.rs
+++ app2/src/main.rs
@@ -3,12 +3,11 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 async fn app2_index(req: HttpRequest) -> impl Responder {
     req.headers().iter().for_each(|(k, v)| {
         if k.as_str().to_lowercase() == "authorization" {
-            // test the token
             println!("Auth Header: {}: {:?}", k, v);
         }
     });
-    
-    HttpResponse::Ok().body("App2 currently not secured")
+
+    HttpResponse::Ok().body("App2 is currently not introspecting the token")
 }
 
 #[actix_web::main]
